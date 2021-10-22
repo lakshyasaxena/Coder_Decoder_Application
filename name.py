7a8521c9e18c7f1a57cda9f8480e6ddf2db32e4c
Mayank Chaudhary <49724597+chaudhary19@users.noreply.github.com>
2021-10-22 11:57:02 +0530

    Created name.py
    It helps you to find what is your name and learn your name

---
+++ name.py
@@ -0,0 +1,19 @@
+name = input()
+for i in range(100):
+  for j in range(100):
+    print("Your name is " + name)
+    
+name = input()
+for i in range(100):
+  for j in range(100):
+    print("Your name is " + name)
+    
+name = input()
+for i in range(100):
+  for j in range(100):
+    print("Your name is " + name)
+    
+name = input()
+for i in range(100):
+  for j in range(100):
+    print("Your name is " + name)
